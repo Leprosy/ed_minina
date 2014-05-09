bf4f3f5f8707cb07f7d0f308455bf9e42f546cce
Miguel Rojas <miguel@nimbic.com>
2014-05-09 16:29:25 -0400

    Answer machine

--- index.php
+++ index.php
@@ -9,46 +9,55 @@ $consumer_key = "wk30aGZtbH5Hbu1vY5Is7yI2U";
 $consumer_secret = "ORNx9CPK38Sph8mBnnvufOSa85H93pj2BrYTUOiuLUaqNLDJU5";
 
 
-/* erase session*/
-if (isset($_GET['logout'])) {
-    session_destroy();
-    die('erase');
-}
+/* Functions */
+function getMau($withcc = True) {
+    $maus = array('mamaauuuu...', 'prr...', 'prrr...', 'cruuñaauu...', 'maaaaaau...','mmau...', 'prrrñau...', 'ñau...', 'miaaaau...', 'MIAAAAAAAAUUUU...');
+    $mau = '';
+    $hums = array('@aindir', '@leprosy', '@aindir', '@angebrik', '@_Natilla');
+    $emos = array(':D', 'XD', ':P', 'xD', ':@');
+    $mausP = rand(2, 6);
+    $humsP = rand(0, 30);
+    $emosP = rand(0, 30);
 
+    if ($humsP < 10 && $withcc) {
+        $mau .= $hums[rand(0, count($hums) - 1)] . ' ';
+    }
+
+    for ($i = 0; $i < $mausP; ++$i) {
+        $mau .= $maus[rand(0, count($maus) - 1)];
+    }
+
+    if ($emosP < 10) {
+        $mau .= $emos[rand(0, count($emos) - 1)];
+    }
+
+    return $mau;
+}
 
 /* Query API */
 if (file_exists($app_file) && $access_token = unserialize(file_get_contents($app_file))) {
     $lock = file_get_contents($app_file . '.lock');
 
-    if (time() - $lock < 60) {
-        die('TOO SOON');
+    if (time() - $lock < $app_time) {
+        die('TOO SOON, ME BOY ;)');
     } else {
         $lock = fopen($app_file . '.lock', 'w');
         fwrite($lock, time());
     }
 
-    $mau = array('mamaauuuu...', 'prr...', 'prrr...', 'cruuñaauu...', 'maaaaaau...','mmau...', 'prrrñau...', 'ñau...', 'miaaaau...', 'MIAAAAAAAAUUUU...');
-    $mmau = '';
-    $hums = array('@aindir', '@leprosy', '@aindir', '@angebrik', '@_Natilla');
-    $emos = array(':D', 'XD', ':P');
-    $toks = rand(2, 10);
-    $cc = rand(0, 30);
-    $emoc = rand(0, 30);
-
-    if ($cc < 10) {
-        $mmau .= $hums[rand(0, count($hums) - 1)] . ' ';
-    }
-
-    for ($i = 0; $i < $toks; ++$i) {
-        $mmau .= $mau[rand(0, count($mau) - 1)];
-    }
-
-    if ($emoc < 10) {
-        $mmau .= $emos[rand(0, count($emos) - 1)];
-    }
-
+    $mau = getMau();
     $connection = new \TwitterOAuth\Api($consumer_key, $consumer_secret, $access_token['oauth_token'], $access_token['oauth_token_secret']);
-    var_dump($connection->post('statuses/update', array('status' => $mmau)));
+
+    /* Answer calls */
+    foreach ($connection->get('statuses/mentions_timeline', array()) as $twit) {
+        if (time() - strtotime($twit->created_at) < $app_time) {
+            $mau = '@' . $twit->user->screen_name . ' ' . getMau(false);
+            var_dump("REPLY", $connection->post('statuses/update', array('status' => $mau, 'in_reply_to_status_id' => $twit->id)));
+        }
+    }
+
+    /* Send a tweet */
+    var_dump("POSTING", $connection->post('statuses/update', array('status' => $mau)));
 } else {
     if (isset($_SESSION['oauth_token'])) {
         /* The temp tokens arrived */
